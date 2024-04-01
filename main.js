b7d2724d1382a10e34220b7c1d7f413e89045a96
Stefan Bia <stefanbia0108@gmail.com>
2024-04-01 12:39:51 +0300

    Create adjustable size

--- main.js
+++ main.js
@@ -20,13 +20,51 @@ let clicked = false;
         square.addEventListener('mouseover', function() {
             
             if(clicked)
-                this.classList.add('clicked');
-            
-            // grid.addEventListener('mouseover', onMouseOver);
+                this.classList.add('clicked');            
 
         });
 
            
     }
+const sizeBut = document.querySelector('#chooseSize');
+sizeBut.addEventListener('click', function(){
+    let size = parseInt(prompt('Select wanted size', '16'));
+
+    if(size > 100)
+        size = 100;
+
+    while(grid.firstChild)
+        grid.removeChild(grid.firstChild);
+
+        grid.style.width = `${20 * size}px`;
+        grid.style.height = `${20 * size}px`;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `20px`;
+        square.style.width = `20px`;
+        grid.appendChild(square);
+    
+        square.addEventListener('mousedown', function(){
+            clicked = true;
+            this.classList.add('clicked');
+        });
+            
+        square.addEventListener('mouseup', function(){
+            clicked = false;
+        });
+    
+        square.addEventListener('mouseover', function() {
+                
+        if(clicked)
+            this.classList.add('clicked');            
+    
+        });
+    
+               
+    }
+
+});
 
     
