64ebb6e7173fb8b9c4e9d4489e381606ce39af3c
Stefan Bia <stefanbia0108@gmail.com>
2024-04-01 12:23:21 +0300

    Create sketch functionality on the grid

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 
 const grid = document.querySelector('.grid');
 
+let clicked = false;
 
     for (let i = 0; i < 16 * 16; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         grid.appendChild(square);
+
+        square.addEventListener('mousedown', function(){
+            clicked = true;
+            this.classList.add('clicked');
+        });
+        
+        square.addEventListener('mouseup', function(){
+            clicked = false;
+        });
+
+        square.addEventListener('mouseover', function() {
+            
+            if(clicked)
+                this.classList.add('clicked');
+            
+            // grid.addEventListener('mouseover', onMouseOver);
+
+        });
+
+           
     }
+
+    
